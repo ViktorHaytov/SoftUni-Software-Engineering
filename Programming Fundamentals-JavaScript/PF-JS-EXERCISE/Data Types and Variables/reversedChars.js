56115d7bc3c2a70239d71c780db0625532c63554
Viktor Haytov <106112206+ViktorHaytov@users.noreply.github.com>
2022-08-30 21:01:40 +0200

    Delete Programming Fundamentals-JavaScript directory

--- Programming Fundamentals-JavaScript/PF-JS-EXERCISE/Data Types and Variables/reversedChars.js
+++
@@ -1,6 +0,0 @@
-function reversedChars (param1, param2, param3) {
-
-    console.log(`${param3} ${param2} ${param1}`);
-}
-
-reversedChars ("A", "B", "C");
--- Programming Fundamentals-JavaScript/PF-JS-EXERCISE/Functions/smallestOfThreeNumbers.js
+++
@@ -1,9 +0,0 @@
-function smallestOfThreeNumbers (numOne, numTwo, numThreee) {
-
-    let smallestNum = Math.min(numOne, numTwo, numThreee);
-
-    return smallestNum;
-
-}
-
-console.log(smallestOfThreeNumbers(2, 5, 3));
