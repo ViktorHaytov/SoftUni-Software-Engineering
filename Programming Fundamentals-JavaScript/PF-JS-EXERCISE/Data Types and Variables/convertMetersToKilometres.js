6ae2fbfff18748a3cd442edc74fa9242dcaef9e4
Viktor Haytov <106112206+ViktorHaytov@users.noreply.github.com>
2022-08-31 18:55:06 +0200

    Delete Programming Fundamentals-JavaScript directory

--- Programming Fundamentals-JavaScript/PF-JS-EXERCISE/Data Types and Variables/convertMetersToKilometres.js
+++
@@ -1,8 +0,0 @@
-function convertMetersToKilometres (meters) {
-
-    let metersToKilometres = meters / 1000;
-
-    console.log(metersToKilometres.toFixed(2));
-}
-
-convertMetersToKilometres (1852);
