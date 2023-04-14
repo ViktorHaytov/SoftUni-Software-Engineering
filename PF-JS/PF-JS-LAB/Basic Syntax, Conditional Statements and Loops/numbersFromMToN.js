41acaef67e121122133ac35a21633d6957f82fcb
viktorhaytov <viktorhaytov90@gmail.com>
2023-04-14 13:08:11 +0200

    My software education

--- PF-JS/PF-JS-LAB/Basic Syntax, Conditional Statements and Loops/numbersFromMToN.js
+++
@@ -1,8 +0,0 @@
-function numbersFromMToN (numM,numN) {
-
-    for (let i = numM; i >= numN; i--) {
-        console.log(i);
-    }
-}
-
-numbersFromMToN (6,2);
--- PF-JS/PF-JS-LAB/Text Processing/substring.js
+++
@@ -1,9 +0,0 @@
-function substring (inputString, startIndex, count) {
-
-    let endIndex = startIndex + count;
-
-    let string = inputString.substring(startIndex, endIndex)
-    console.log(string);
-}
-
-substring ('ASentence', 1, 8);
